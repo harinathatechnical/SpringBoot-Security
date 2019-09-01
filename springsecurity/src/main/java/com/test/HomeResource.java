0acdf21e96c71928b8a2ef9d257330f82ba1c121
harinathatechnical <47938954+harinathatechnical@users.noreply.github.com>
2019-09-01 15:06:42 +0530

    Update HomeResource.java

--- springsecurity/src/main/java/com/test/HomeResource.java
+++ springsecurity/src/main/java/com/test/HomeResource.java
@@ -6,8 +6,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
+    /**
+     * this is able to access by everyone
+     * @return
+     */
+    
     @GetMapping("/")
     public String home(){
         return ("<h1>Welcome</h1>");
+    }
+    
+    /**
+     * this is able to access by only admin
+     * @return
+     */
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>Welcome Admin</h1>");
+    }
+
+    /**
+     * this is able to access by admin and user
+     * @return
+     */
+    @GetMapping("/user")
+    public String user(){
+        return ("<h1>Welcome User</h1>");
     }
 }
